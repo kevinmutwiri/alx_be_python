abb94b63925a6b88b7d5cba875ef698fda6babf5
kevinmutwiri <keivnmutwiri@gmail.com>
2025-05-25 15:05:41 +0300

    Deal with float inputs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
+income = int(float((input("Enter your monthly income: "))))
+expenses = int(float(input("Enter your total monthly expenses: ")))
 
 savings = income - expenses
-projected_savings = int(savings * 12 + (savings * 12 * 0.05))
+projected_savings = savings * 12 + int((savings * 12 * 0.05))
 
 print(f"Your monthly savings are ${savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
