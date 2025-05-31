6ad9411219d5b08625a876b1079c2010d50b607f
kevinmutwiri <keivnmutwiri@gmail.com>
2025-05-31 20:16:22 +0300

    Implement a simple calculator

--- control-flow/README.md
+++ control-flow/README.md
@@ -3,3 +3,4 @@
 > [home](../README.md)
 
 - [weather_advice.py](./weather_advice.py) - Recommend action based on weather.
+- [match_case_calculator.py](./match_case_calculator.py) - Implement simple calculator.
---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match (operation):
+    case '+':
+        print("The result is {num1 + num2}.")
+    case '-':
+        print("The result is {num1 - num2}.")
+    case '*':
+        print("The result is {num1 * num2}.")
+    case '/':
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            print("Cannot divide by zero.")
+        else:
+            print("The result is {num1 / num2}.")
