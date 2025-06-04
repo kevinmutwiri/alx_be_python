fbae31384015b19ff6371231de0a52a3d86c5ded
kevinmutwiri <keivnmutwiri@gmail.com>
2025-06-04 11:16:43 +0300

    Use elif to see if checker will pass

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 != 0:
                 return num1 / num2
-            else:
+            elif num2 == 0:
                 return "Cannot divide by zero."
 
 if __name__ == "__main__":
