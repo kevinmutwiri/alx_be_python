e7f5f43494fbbb5c96ee8a3c194f014a1fcd69a4
kevinmutwiri <keivnmutwiri@gmail.com>
2025-06-06 08:29:58 +0300

    Implement temperature conversion tool

--- fns_and_dsa/README.md
+++ fns_and_dsa/README.md
@@ -4,3 +4,4 @@
 
 - [arithmetic_operations.py](./arithmetic_operations.py) - Implement a function that performs basic arithmetic operations.
 - [shopping_list_manager.py](./shopping_list_manager.py) - Implement a shopping list manager.
+- [temp_conversion_tool.py](./temp_conversion_tool.py) - Implement a temperature conversion tool.
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,36 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+
+def main():
+    while True:
+        temperature = input("Enter the temperature to convert: ")
+        if not temperature.isdigit():
+            print("Invalid temperature. Please enter a numeric value")
+            continue
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
+        if unit not in ('c', 'f'):
+            print("Please input either 'C' or 'F'")
+            continue
+        else:
+            break
+    temperature = int(temperature)
+    match unit:
+        case 'f':
+            converted = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {converted}°C")
+        case 'c':
+            converted = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {converted}°F")
+
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+def convert_to_fahrenheit(celsius):
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+
+
+if __name__ == "__main__":
+    main()
